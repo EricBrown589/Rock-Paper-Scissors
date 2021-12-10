62279a79e5dfb815ea5cd804877a761fc05cd357
Eric Brown <ericbrown589@yahoo.com>
2021-12-10 16:11:11 -0600

    added a 'please enter input' line

--- src/Game.java
+++ src/Game.java
@@ -2,6 +2,7 @@ import java.util.Random;
 
 public class Game {
     public static void main(String[] args) {
+        System.out.println("Please enter Rock, Paper, or Scissors");
         User user = new User();
         user.choice();
         Computer comp = new Computer();
